7f094ee9582230341cea38411cedeca8f7ff8524
Kelsey Bowen <kelseybowen22@gmail.com>
2025-07-18 13:28:06 -0700

    api calls and db functions

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ import json
 import pymysql
 import time
 from models import User, Destination, Search
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OW_API_KEY = os.getenv('OPEN_WEATHER_API_KEY')
 
 
 zt_logo = """
@@ -33,12 +39,12 @@ def welcome_user():
     clear_console()
 
     print(zt_logo)
-    welcome_message = """Menu: \n[1] New User - Register \n[2] Returning User - Login \n[3] Search as Guest \n[Q] Quit"""
+    welcome_message = """------ Menu ------  \n[1] New User - Register \n[2] Returning User - Login \n[3] Search as Guest \n[Q] Quit\n------------------"""
     # app_description = """
     # Search for travel destinations and view popular
     # restaurants and attractions, weather forecasts,
     # and packing list recommendations all in one place."""
-    app_description = """Enter a zip code to see the city name and weather forecast. \nSearch as a guest or create a profile to save destinations. \n\nCreating a profile takes less than 30 seconds and allows you \nto save destinations for your future travels.
+    app_description = """Enter a valid US zip code to see the city name and weather forecast. \nSearch as a guest or create a profile to save destinations. \n\nCreating a profile takes less than 30 seconds and allows you \nto save destinations for your future travels.
     """
     print(app_description)
     print(welcome_message)
@@ -61,7 +67,7 @@ def new_user():
     clear_console()
     print(zt_logo)
     print("""Welcome New User!\n""")
-    menu = """Menu: \n[1] Create Profile \n[0] Go Back"""
+    menu = """------ Menu ------  \n[1] Create Profile \n[0] Go Back\n------------------"""
     print(f"{menu}\n")
     choice = input("""Make a selection: """)
     if choice == "1":
@@ -79,16 +85,16 @@ def user_dashboard(user):
     print(zt_logo)
     if user == "Guest":
         print("\nWelcome, Guest!\n")
-        menu = """Menu: \n[1] Destination Search \n[0] Go Back"""
+        menu = """------ Menu ------  \n[1] Destination Search \n[0] Go Back\n------------------"""
     else:
         print(f"""{user}'s Dashboard\n""")
-        menu = """Menu: \n[1] Destination Search \n[2] View Saved Destinations \n[0] Go Back"""
+        menu = """------ Menu ------  \n[1] Destination Search \n[2] View Saved Destinations \n[0] Go Back\n------------------"""
     print(f"{menu}\n")
     choice = input("""Make a selection: """)
     if choice == "1":
-        destination_search()
+        destination_search(user)
     elif choice == "2":
-        view_saved_destinations()
+        view_saved_destinations(user)
     elif choice == "0":
         welcome_user()
 
@@ -111,8 +117,8 @@ def create_profile():
         confirm = input("\nIs this correct? y/n: ")
         print("\n")
         if confirm.lower() == 'y':
-            data = {'name': new_user}
-            User.create_user(data)
+            user_data = {'name': new_user}
+            User.create_user(user_data)
             user_dashboard(new_user)
         elif confirm.lower() == 'n':
             create_profile()
@@ -122,66 +128,87 @@ def create_profile():
 
 
 # DESTINATION SEARCH
-def destination_search(first_run=True):
+def destination_search(user, first_run=True):
     if first_run:
         clear_console()
         print(zt_logo)
     print("""Destination Search\n""")
-    menu = """Menu: \n[1] Enter Zip Code \n[0] Go Back"""
+    menu = """------ Menu ------  \n[1] Enter US Zip Code \n[0] Go Back\n------------------"""
     print(f"{menu}\n")
     choice = input("""Make a selection: """)
     if choice == "1":
-        zip = input("Enter a zip code to search: ")
-        search_by_zip(zip)
+        zip = input("Enter a valid US zip code to search: ")
+        dest_info = get_destination_name_from_zip(zip)
+        print("""Search Results:\n""")
+        print(f"You searched for: {zip}\n")
+        print(dest_info[1])
+        if dest_info[0]: # valid search
+            user_choice_save = input("Save this search? y/n: ")
+            if user_choice_save.lower() == 'y':
+                save_search_to_db(dest_info[1], user)
+        else: # invalid search
+            destination_search(user, first_run=False)
     elif choice == "0":
-        user_dashboard()
+        user_dashboard(user)
     else:
         print("Invalid entry. Please try again.")
-        destination_search(first_run=False)
+        destination_search(user, first_run=False)
 
-
-# SEARCH RESULTS
-def search_by_zip(zip):
-    clear_console()
-    print("""Search Results\n""")
-    search_info = f"You searched for: {zip}"
-    # city_name = "Boston, MA"
-    menu = """Menu: \n[1] Save Search \n[2] Show Details \n[0] Go Back"""
-    print(f"{search_info}\n")
-    print(f"{city_name}\n")
-    print(f"{menu}\n")
-    choice = input("Make a selection: ")
-    if choice == "1":
-        save_search()
-    elif choice == "0":
-        destination_search()
+def get_destination_name_from_zip(zip):
+    req = requests.get(f'http://api.openweathermap.org/geo/1.0/zip?zip={zip},US&appid={OW_API_KEY}')
+    if req.status_code != 200:
+        return (False, "Invalid zip code. Please enter a valid US zip code.")
     else:
-        print("Invalid entry. Please try again.")
-        search_by_zip()
-
+        result = req.json()
+        return (True, result)
 
 # SAVE SEARCH
-def save_search():
+def save_search_to_db(dest_info, user):
     clear_console()
-    print("\nSave Search Results\n")
-    print("Your search results for Boston, MA have been saved.")
-    menu = """Menu: \n[0] Return to Dashboard \n[Q] Quit"""
+    dest_data = {'zip': dest_info['zip'], 'name': dest_info['name'], 'lat': dest_info['lat'], 'lon': dest_info['lon']}
+    saved_dest = Destination.save_destination(dest_data)
+    user_data = {'name': user}
+    user_id = User.get_user_id_by_name(user_data)[0]['id']
+    if saved_dest:
+        search_data = {'user_id': user_id, 'destination_id': saved_dest}
+        saved_search = Search.save_search(search_data)
+        print(f"Your search results for {dest_info['zip']}: {dest_info['name']} have been saved.")
+    else:
+        print("There was a problem saving your destination.")
+    menu = """------ Menu ------  \n[0] Return to Dashboard \n[Q] Quit\n------------------"""
     print(f"\n{menu}\n")
     choice = input("Make a selection: ")
     if choice == "Q" or choice == "q":
         pass
-    elif choice == "1":
-        user_dashboard()
+    elif choice == "0":
+        user_dashboard(user)
     else:
         print("Invalid entry. Please try again.")
-        save_search()
 
 
 # VIEW SAVED DESTINATIONS
 def view_saved_destinations(user):
-    pass
+    clear_console()
+    print(zt_logo)
     print(f"\n{user}'s Saved Destinations\n")
+    user_data = {'name': user}
+    user_id = User.get_user_id_by_name(user_data)[0]['id']
+    data = {'user_id': user_id}
+    saved_searches = Search.get_user_saved_searches(data)
+    for i in range(len(saved_searches)):
+        print(f"[{i+1}] {saved_searches[i]['name']}: {saved_searches[i]['zip']}")
+    selection = input("\nSelect a Saved Destination to view details, or 0 to return to Dashboard: ")
+    if selection == '0':
+        user_dashboard(user)
+    elif selection.isdigit() and int(selection) in range(len(saved_searches)):
+        display_details(saved_searches[int(selection)-1], user)
+    else:
+        print("Invalid entry. Please try again.")
+        view_saved_destinations(user)
 
+def display_details(destination, user):
+    print(f"Details for {destination['name']}\n")
+    
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -3,54 +3,68 @@ from db_connector import connectToMySQL
 
 class User:
     db = "361_main"
-    def __init__(self, data):
-        self.id = data['id']
-        self.name = data['name']
+    def __init__(self, user_data):
+        self.id = user_data['id']
+        self.name = user_data['name']
     
     @classmethod
-    def create_user(cls, data):
-        query = "INSERT INTO Users ( name ) VALUES ( %(name)s );"
-        return connectToMySQL(cls.db).query_db(query, data)
+    def create_user(cls, user_data):
+        query = "INSERT INTO Users (name) VALUES ( %(name)s );"
+        return connectToMySQL(cls.db).query_db(query, user_data)
+    
+    @classmethod
+    def get_user_by_name(cls, user_data):
+        query = "SELECT * FROM Users WHERE name = %(name)s;"
+        return connectToMySQL(cls.db).query_db(query, user_data)
 
     @classmethod
-    def get_user_by_name(cls, data):
-        query = "SELECT * FROM Users WHERE name = %(name)s;"
-        return connectToMySQL(cls.db).query_db(query, data)
+    def get_user_id_by_name(cls, user_data):
+        query = "SELECT id FROM Users WHERE name = %(name)s;"
+        return connectToMySQL(cls.db).query_db(query, user_data)
 
 
 class Destination:
     db = "361_main"
-    def __init__(self, data):
-        self.id = data['id']
-        self.zip_code = data['zip_code']
-        self.city_name = data['city_name']
+    def __init__(self, dest_data):
+        self.id = dest_data['id']
+        self.zip = dest_data['zip']
+        self.name = dest_data['name']
+        self.lat = dest_data['lat']
+        self.lon = dest_data['lon']
 
     @classmethod
-    def create_destination(cls, data):
-        query = "INSERT INTO Destinations (zip_code, city_name) VALUES (%(zip_code)s, %(city_name)s);"
-        return connectToMySQL(cls.db).query_db(query, data)
+    def save_destination(cls, dest_data):
+        query = "INSERT INTO Destinations (zip, name, lat, lon) VALUES (%(zip)s, %(name)s, %(lat)s, %(lon)s);"
+        return connectToMySQL(cls.db).query_db(query, dest_data)
     
     @classmethod
-    def get_destination_by_zip(cls, data):
-        query = "SELECT * FROM Destinations WHERE zip_code = %(zip_code)s;"
-        return connectToMySQL(cls.db).query_db(query, data)
+    def get_destination_by_zip(cls, dest_data):
+        query = "SELECT * FROM Destinations WHERE zip = %(zip)s;"
+        return connectToMySQL(cls.db).query_db(query, dest_data)
 
     @classmethod
-    def delete_destination(cls, data):
+    def delete_destination(cls, dest_data):
         query = "DELETE FROM Destinations WHERE id = %(id)s;"
-        return connectToMySQL(cls.db).query_db(query, data)
+        return connectToMySQL(cls.db).query_db(query, dest_data)
 
 
 class Search:
     db = "361_main"
-    def __init__(self, data):
-        self.user_id = data['user_id']
-        self.destination_id = data['destination_id']
+    def __init__(self, search_data):
+        self.user_id = search_data['user_id']
+        self.destination_id = search_data['destination_id']
 
-    def save_search(cls, data):
+    @classmethod
+    def save_search(cls, search_data):
         query = "INSERT INTO Searches (user_id, destination_id) VALUES (%(user_id)s, %(destination_id)s);"
+        return connectToMySQL(cls.db).query_db(query, search_data)
+
+    @classmethod
+    def get_user_saved_searches(cls, data):
+        query = "SELECT zip, name, lat, lon FROM Destinations JOIN Searches ON destination_id = Destinations.id WHERE user_id = %(user_id)s;"
         return connectToMySQL(cls.db).query_db(query, data)
 
-    def delete_search(cls, data):
+    @classmethod
+    def delete_search(cls, search_data):
         query = "DELETE FROM Searches WHERE id = %(id)s;"
-        return connectToMySQL(cls.db).query_db(query, data)
+        return connectToMySQL(cls.db).query_db(query, search_data)
