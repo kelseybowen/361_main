bceacc59a16217a4f50c353ea85602b14c76bfac
Kelsey Bowen <kelseybowen22@gmail.com>
2025-07-27 19:11:53 -0700

    added timezone service

--- main.py
+++ main.py
@@ -10,11 +10,30 @@ import time
 from models import User, Destination, Search
 import requests
 from dotenv import load_dotenv
+import zmq
 
 load_dotenv()
 
-OW_API_KEY = os.getenv('OPEN_WEATHER_API_KEY')
+# --------------------------- SOCKET SETUP ------------------------------
 
+# SOCKET FOR TIME ZONE SERVICE
+tz_context = zmq.Context()
+tz_socket = tz_context.socket(zmq.REQ)
+tz_socket.connect("tcp://localhost:5555")
+
+# SOCKET FOR PLACES SERVICE
+places_context = zmq.Context()
+places_socket = places_context.socket(zmq.REQ)
+places_socket.connect("tcp://localhost:5556")
+
+# SOCKET FOR WEATHER SERVICE
+weather_context = zmq.Context()
+weather_socket = weather_context.socket(zmq.REQ)
+weather_socket.connect("tcp://localhost:5557")
+# ------------------------------------------------------------------------
+
+
+OW_API_KEY = os.getenv('OPEN_WEATHER_API_KEY')
 
 zt_logo = """
         _____   _          ______     _     
@@ -73,21 +92,6 @@ def returning_user():
         else:
             welcome_user()
 
-
-# NEW USER
-def new_user():
-    display_title("""Welcome New User!\n""")
-    menu = """------ Menu ------  \n[C] Create Profile \n[M] Back to Main Menu\n------------------"""
-    print(f"{menu}\n")
-    choice = input("""Make a selection: """)
-    if choice.lower() == "c":
-        create_profile()
-    elif choice.lower() == "m":
-        welcome_user()
-    else:
-        print("Invalid choice")
-        new_user()
-
 # CREATE PROFILE
 def create_profile():
     display_title("""Welcome New User!\n""")
@@ -119,16 +123,16 @@ def create_profile():
         welcome_user()
     else:
         print("Invalid choice")
-        new_user()
+        create_profile()
 
 # USER DASHBOARD
 def user_dashboard(user, new_profile=False):
     if user == "Guest":
         dash_title = "\nWelcome, Guest!\n"
-        menu = """------ Menu ------  \n[S] Destination Search \n[M] Main Menu\n------------------"""
+        menu = """------ Menu ------  \n[S] Destination Search \n[M] Back to Main Menu\n------------------"""
     else:
         dash_title = f"""{user}'s Dashboard\n"""
-        menu = """------ Menu ------  \n[S] Destination Search \n[V] View Saved Destinations \n[M] Main Menu\n------------------"""
+        menu = """------ Menu ------  \n[S] Destination Search \n[V] View Saved Destinations \n[L] Log Out\n------------------"""
     display_title(dash_title)
     if new_profile:
         print(f"Profile successfully created for {user}\n")
@@ -138,33 +142,32 @@ def user_dashboard(user, new_profile=False):
         destination_search(user)
     elif choice.lower() == "v":
         view_saved_destinations(user)
-    elif choice.lower() == "m":
+    elif choice.lower() == "m" or choice.lower() == "l":
         welcome_user()
     else:
         user_dashboard(user)
-
 
 # DESTINATION SEARCH
 def destination_search(user, first_run=True):
     if first_run:
         display_title("""Destination Search\n""")
-    menu = """------ Menu ------  \n[Z] Search by Zip Code \n[C] Search by City Name \n[M] Main Menu\n------------------"""
+    menu = """------ Menu ------  \n[Z] Search by Zip Code \n[C] Search by City Name \n[R] Return to Dashboard\n------------------"""
     print(f"{menu}\n")
     choice = input("""Make a selection: """)
     if choice.lower() == "z":
         zip = input("Enter a valid US zip code to search: ")
-        dest_info = get_destination_name_from_zip(zip)
-        print("""Search Results:\n""")
-        print(f"You searched for: {zip}\n")
-        for key, value in dest_info[1].items():
-            print(f"{key.title()}: {value}")
-        # print(dest_info[1])
-        if dest_info[0]: # valid search
-            user_choice_save = input("\nSave this search? y/n: ")
-            if user_choice_save.lower() == 'y':
-                save_search_to_db(dest_info[1], user)
-            else:
-                destination_search(user)
+        if len(zip) == 5 and zip.isnumeric():
+            dest_info = get_destination_name_from_zip(zip)
+            print("""Search Results:\n""")
+            print(f"You searched for: {zip}\n")
+            for key, value in dest_info[1].items():
+                print(f"{key.title()}: {value}")
+            if dest_info[0]: # valid search
+                user_choice_save = input("\nSave this search? y/n: ")
+                if user_choice_save.lower() == 'y':
+                    save_search_to_db(dest_info[1], user)
+                else:
+                    destination_search(user)
         else: # invalid search
             print("Invalid entry. Please try again.")
             destination_search(user, first_run=False)
@@ -177,6 +180,7 @@ def destination_search(user, first_run=True):
         print("""Search Results:\n""")
         if dest_info: # valid search
             print(f"You searched for: {city, state}\n")
+            print(f"destination info ========= {dest_info}")
             display_details(dest_info[1])
             user_choice_save = input("Save this search? y/n: ")
             if user_choice_save.lower() == 'y':
@@ -186,7 +190,7 @@ def destination_search(user, first_run=True):
             redirect_to_zip_input = input(f"Your search for {city}, {state} did not return any results. Try again using a zip code instead? y/n: ")
             if redirect_to_zip_input.lower() == "y":
                 destination_search(user, first_run=False)
-    elif choice.lower() == "m":
+    elif choice.lower() == "r":
         user_dashboard(user)
     else:
         print("Invalid entry. Please try again.")
@@ -247,10 +251,10 @@ def view_saved_destinations(user):
     # USER HAS NO SAVED SEARCHES
     if len(saved_searches) == 0:
         print("No Saved Searches to display.\n")
-        menu = """\n------ Menu ------  \n[D] Return to Dashboard \n[Q] Quit\n------------------"""
+        menu = """\n------ Menu ------  \n[R] Return to Dashboard \n[Q] Quit\n------------------"""
         print(f"{menu}\n")
         selection = (input("Make a selection: "))
-        if selection.lower() == 'd':
+        if selection.lower() == 'r':
             user_dashboard(user)
         elif selection.lower() == 'q':
             pass
@@ -280,7 +284,6 @@ def view_saved_destinations(user):
             search_to_view = input("\nEnter the number of the Destination you would like to see details for: ")
             if search_to_view.isdigit():
                 saved_search_details = Destination.get_destination_by_id({'id': int(search_to_view)})
-                print(saved_search_details)
                 display_details(saved_search_details[0])
                 nav_away_from_details = input("Enter 'S' to return to your Saved Destinations, or any other key to Quit: ")
                 if nav_away_from_details.lower() == 's':
@@ -293,21 +296,8 @@ def view_saved_destinations(user):
         
         # DELETE SAVED SEARCH
         elif selection.lower() == 'd':
-            display_title(f"{user}'s Saved Destinations:\n")
-            for i in range(len(saved_searches)):
-                print(f"[{saved_searches[i]['id']}] {saved_searches[i]['name']}: {saved_searches[i]['zip']}")
-            search_to_delete = input("\nSelect the search you'd like to delete: ")
-            if search_to_delete.isdigit():
-                confirm_delete = input(f"Are you sure you want to delete your saved search for {saved_searches[i]['name']}? This action cannot be undone. y/n: ")
-                if confirm_delete.lower() == 'y':
-                    search_data = {'user_id': user_id, 'destination_id': int(search_to_delete)}
-                    Search.delete_search(search_data)
-                    view_saved_destinations(user)
-                else:
-                    view_saved_destinations(user)
-            else:
-                print("Invalid entry. Please try again.")
-                view_saved_destinations(user)
+            delete_saved_destination(user, user_id, saved_searches)
+                
         elif selection.lower() == "q":
             print("Goodbye\n")
             pass
@@ -315,12 +305,99 @@ def view_saved_destinations(user):
             print("Invalid entry. Please try again.")
             view_saved_destinations(user)
 
+# DELETE SAVED DESTINATION
+def delete_saved_destination(user, user_id, saved_searches):
+    display_title(f"{user}'s Saved Destinations:\n")
+    for i in range(len(saved_searches)):
+        print(f"[{saved_searches[i]['id']}] {saved_searches[i]['name']}: {saved_searches[i]['zip']}")
+    search_to_delete = input("\nSelect the search you'd like to delete: ")
+    if search_to_delete.isdigit():
+        confirm_delete = input(f"Are you sure you want to delete your saved search for {saved_searches[i]['name']}? This action cannot be undone. y/n: ")
+        if confirm_delete.lower() == 'y':
+            search_data = {'user_id': user_id, 'destination_id': int(search_to_delete)}
+            Search.delete_search(search_data)
+            view_saved_destinations(user)
+        else:
+            view_saved_destinations(user)
+    else:
+        print("Invalid entry. Please try again.")
+        view_saved_destinations(user)
+
 def display_details(destination):
-    display_title("Search Details\n")
-    if 'zip' in destination:
-        print(f"Details for {destination['name']}: \nZip code: {destination['zip']} \nLatitude: {destination['lat']} \nLongitude: {destination['lon']}\n\n")
+    display_title(f"Search Details for {destination['name']}\n")
+    if 'zip' in destination and destination['zip'] != None:
+        print(f"Zip code: {destination['zip']} \nLatitude: {destination['lat']} \nLongitude: {destination['lon']}\n\n")
+        get_time_zone(destination)
     else:
         print(f"Details for {destination['name']}: \nLatitude: {destination['lat']} \nLongitude: {destination['lon']}\n\n")
+        print(f"Time zone data not available for {destination['name']}\n")
+
+def print_search_details_page_menu(user):
+    menu = "------ Menu ------  \n[R] Restaurant Search \n[W] Weather \n[S] Back to Saved Destinations \n[D] Back to Dashboard\n------------------"
+    print(menu)
+    choice = input("Make a selection: ")
+    if choice.lower() == "r":
+        pass
+    elif choice.lower() == "w":
+        pass
+    elif choice.lower() == "s":
+        view_saved_destinations(user)
+    elif choice.lower() == "d":
+        user_dashboard(user)
+        
+
+
+# ------------------------ TIME ZONE SERVICE ----------------------------
+def get_time_zone(dest):
+    zip = dest['zip']
+    name = dest['name']
+    s_zip = str(zip)
+    tz_socket.send_string(s_zip)
+    time_zone_data = tz_socket.recv()
+    decoded_data = json.loads(time_zone_data.decode())
+    print_timezone_info(decoded_data, name)
+    
+def create_tz_diff_message(tz_data):
+    if tz_data["time_difference"] == 0:
+        diff_message = "the same as your local time"
+    elif tz_data["time_difference"] == 1:
+        diff_message = "1 hour ahead of your local time"
+    elif tz_data["time_difference"] > 1:
+        diff_message = f"{tz_data['time_difference']} hours ahead of your local time"
+    else:
+        diff_message = f"{abs(tz_data['time_difference'])} hours behind your local time"
+    return diff_message
+
+def print_timezone_info(tz_data, name):
+    diff_message = create_tz_diff_message(tz_data)
+    hm_format = tz_data['hm_format']
+    tz_abbreviation = tz_data['tz_abbreviation']
+    local_time = tz_data['local_time']
+    print("-------- TIME ZONE INFORMATION --------")
+    print(f"Current Time in {name}: {hm_format} ({tz_abbreviation})")
+    print(f"Your Local Time: {local_time}")
+    print(f"{name} is {diff_message}.")
+    print("---------------------------------------\n")
+
+# ------------------------------------------------------------------------
+
+
+# ------------------------- PLACES SERVICE -------------------------------
+# for place in details:
+#     print(f"ID: {place['id']}")
+#     print(f"Name: {place['name']}")
+#     if place['description'] != None:
+#         print(f"Description: {place['description']['text']}")
+#     print("\n")
+
+
+# ------------------------------------------------------------------------
+
+
+# ------------------------- WEATHER SERVICE -------------------------------
+
+
+# ------------------------------------------------------------------------
 
 if __name__ == "__main__":
     welcome_user()
